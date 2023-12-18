067c26c8752b6db771446f074fe83932f8096f25
Muhammad Widyantoro Wiryawan <153991554+m-widyantoro-w-121140183@users.noreply.github.com>
2023-12-18 08:38:40 +0700

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -8,20 +8,6 @@ function setCookie(name, value, days) {
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
-}
 
 function saveToLocalStorage(data) {
     localStorage.setItem('userData', JSON.stringify(data));
